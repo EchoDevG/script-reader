22e6c27913fc63df66cd3dd862b71e502dd9e500
EchoDevG <75668023+EchoDevG@users.noreply.github.com>
2022-02-07 16:27:40 +0000

    Add files via upload

---
+++ script reader/script reader/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace script_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.playpause = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(12, 12);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(120, 81);
+            this.openFile.TabIndex = 0;
+            this.openFile.Text = "open file";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(591, 385);
+            this.textBox1.TabIndex = 1;
+            // 
+            // playpause
+            // 
+            this.playpause.Location = new System.Drawing.Point(8, 486);
+            this.playpause.Name = "playpause";
+            this.playpause.Size = new System.Drawing.Size(518, 54);
+            this.playpause.TabIndex = 2;
+            this.playpause.Text = "play/pause";
+            this.playpause.UseVisualStyleBackColor = true;
+            this.playpause.Click += new System.EventHandler(this.playpause_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(138, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(464, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(138, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(464, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 548);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.playpause);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button playpause;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ script reader/script reader/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+using Microsoft.CSharp;
+
+
+namespace script_reader
+{
+    public partial class Form1 : Form
+    {
+
+        public bool play = false;
+        public bool hasPlayed = false;
+        public SpeechSynthesizer voice = new SpeechSynthesizer();
+        public string textString;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (InstalledVoice iVoice in voice.GetInstalledVoices())
+            {
+                VoiceInfo info = iVoice.VoiceInfo;
+                comboBox1.Items.Add(info.Name);
+            }
+
+            voice.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(voice_SpeakCompleted);
+
+            voice.Volume = trackBar1.Value;
+
+            //voice.Rate = 1;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Documents\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+            }
+
+            try
+            {
+                textString = File.ReadAllText(filePath);
+            }
+            catch{}
+
+
+            textBox1.Text = textString;
+
+            this.ActiveControl = null;
+
+        }
+
+
+
+        private void playpause_Click(object sender, EventArgs e)
+        {
+            this.ActiveControl = null;
+            playpauseFunc();
+            
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    { playpauseFunc();}
+                    break;
+                default: { break; }
+            }
+        }
+
+
+        
+        public void playpauseFunc()
+        {
+            
+            switch (play) { 
+                case true:
+                    {
+                        voice.Pause();
+                        play = false;
+                        break;
+                    }
+                case false:
+                    {
+                        if (hasPlayed == false)
+                        {
+                            voice.SpeakAsync(textString);
+                            hasPlayed = true;
+                            comboBox1.Enabled = false;
+                            return;
+                        }
+                        voice.Resume();
+                        play = true;
+
+                        break;
+                    }
+                default: { break; }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            voice.Volume = trackBar1.Value;
+            this.ActiveControl = null;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            voice.SelectVoice((string)comboBox1.SelectedItem);
+            this.ActiveControl = null;
+        }
+
+        public void voice_SpeakCompleted(object sender, EventArgs e)
+        {
+            hasPlayed = false;
+            comboBox1.Enabled = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            hasPlayed = false;
+            play = false;
+            comboBox1.Enabled = true;
+            voice.SpeakAsyncCancelAll();
+        }
+    }
+}
